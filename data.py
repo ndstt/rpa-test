8d49e22cd122ea5e72ffc54d798be7fd2eb7e72b
Natdanai Sutampawadee <27091980-ndstt@users.noreply.gitlab.com>
2026-05-17 23:12:30 +0700

    CourseDetail CourseCategory InstructorDetail

--- data.py
+++ data.py
@@ -1,5 +1,4 @@
 from typing import TypedDict
-from datetime import datetime
 
 class CourseDetail(TypedDict):
     url: str # use as primary key
@@ -7,10 +6,13 @@ class CourseDetail(TypedDict):
     cost: int # in THB
     overview: str
     instructor_name: str
-    instructor_position: str # can be empty if the instructor are company or doesn't have position
     avg_rating: float # from 0 to 5
     total_reviews: int
 
 class CourseCategory(TypedDict):
     course_url: str
     category: str
+
+class InstructorDetail(TypedDict):
+    name: str
+    position: str
